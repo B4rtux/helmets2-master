a6a4d214756f49e83e99df0d93d1cc6d89732058
B4rtux <35530977+B4rtux@users.noreply.github.com>
2022-05-30 16:14:59 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,351 +2,101 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
-
-import * as SyncFix from './sync-fix';
-
-const fetch = require('node-fetch');
-
-const DEBUG = false;
-
+// import { MreArgumentError } from '@microsoft/mixed-reality-extension-sdk';
 /**
- * The structure of a hat entry in the hat database.
+ * The main class of this app. All the logic goes here.
  */
-type HatDescriptor = {
-    resourceId: string;
-    attachPoint: string;
-    scale: {
-        x: number;
-        y: number;
-        z: number;
-    };
-    rotation: {
-        x: number;
-        y: number;
-        z: number;
-    };
-    position: {
-        x: number;
-        y: number;
-        z: number;
-    };
-    menuScale: {
-        x: number;
-        y: number;
-        z: number;
-    };
-    menuRotation: {
-        x: number;
-        y: number;
-        z: number;
-    };
-    menuPosition: {
-        x: number;
-        y: number;
-        z: number;
-    };
-    previewMargin: number;
-};
 
-/**
- * WearAHat Application - Showcasing avatar attachments.
- */
-export default class WearAHat {
-    // Container for primitives
-    private assets: MRE.AssetContainer;
+//tt 
+export default class HelloWorld {
+	private text: MRE.Actor = null;
+	private button: MRE.Actor = null;
+	private player: MRE.Actor = null;
+	private assets: MRE.AssetContainer;
+	private buzzerSound: MRE.Sound = undefined;
 
-    // Container for instantiated hats.
-    private attachedHats = new Map<MRE.Guid, MRE.Actor>();
+	constructor(private context: MRE.Context) {
+		this.context.onStarted(() => this.started());
+	}
 
-    // Load the database of hats.
-    // tslint:disable-next-line:no-var-requires variable-name
-    private HatDatabase: { [key: string]: HatDescriptor } = {};
+	/**
+	 * Once the context is "started", initialize the app.
+	 */
+	private async started() {
+		// set up somewhere to store loaded assets (meshes, textures, animations, gltfs, etc.)
+		this.assets = new MRE.AssetContainer(this.context);
 
-    // Options
-    private previewMargin = 1.5; // spacing between preview objects
+		const menu = MRE.Actor.Create(this.context, {});
 
-    /*
-     * Create a UserSyncFix object to work around Altspace MRE library late joiner bugs.
-     * Parameter is the number if millseconds to wait before attachments are synchronized.  All
-     * users that join within that timeframe with have their synchronizations "batched".
-     */
-    private syncFix: SyncFix.UserSyncFix = new SyncFix.UserSyncFix(5000);
+		// Load a glTF model before we use it
+		const cubeData = await this.assets.loadGltf('Button.glb', "box");
 
-    private controls = '../public/defaults.json';
+		// spawn a copy of the glTF model
+		this.button = MRE.Actor.CreateFromLibrary(this.context, {
+			// using the data we loaded earlier
+			resourceId:"artifact:1956000461060309134",
+			// Also apply the following generic actor properties.
+			actor: {
+				name: 'Altspace button',
+				// Parent the glTF model to the text actor, so the transform is relative to the text
+				parentId: menu.id,
+				transform: {
+					local: {
+						position: { x: 0, y: -1, z: 0 },
+						scale: { x: 0.4, y: 0.4, z: 0.4 }
+					}
+				}
+			}
+		});
 
-    /**
-     * Constructs a new instance of this class.
-     * @param context The MRE SDK context.
-     * @param baseUrl The baseUrl to this project's `./public` folder.
-     */
-    constructor(private context: MRE.Context, private params: MRE.ParameterSet, private baseUrl: string) {
-        this.assets = new MRE.AssetContainer(context);
-
-        // Hook the context events we're interested in.
-        this.context.onStarted(() => {
-            switch(this.params.controls) {
-                case "min":
-                    this.controls = '../public/min.json';
-                    break;
-                case "none":
-                    this.controls = '../public/none.json';
-                    break;
-                default:
-                    break;
-            }
-
-            if(this.params.content_pack){
-                // Specify a url to a JSON file
-                // https://account.altvr.com/content_packs/1187493048011980938
-                // e.g. ws://10.0.1.89:3901?content_pack=1187493048011980938
-
-                fetch('https://account.altvr.com/api/content_packs/' + this.params.content_pack + '/raw.json')
-                    .then((res: any) => res.json())
-                    .then((json: any) => {
-                        if(DEBUG){ console.log(json); }
-                        this.HatDatabase = Object.assign({}, json, require(this.controls));;
-                        this.started();
-                    })
-           }
-            else {
-                // Choose the set of helmets
-                // defaults include actions like Clear, Move Up/Down, and Size Up/Down
-                // e.g. ws://10.0.1.89:3901?kit=city_helmets
-                switch(this.params.kit) {
-                    case "city_helmets": {
-                        this.HatDatabase = Object.assign({}, require('../public/data/1167643861778956427_city_helmets.json'), require(this.controls));
-                        break;
-                    }
-                    case "space_helmets": {
-                        this.HatDatabase = Object.assign({}, require('../public/data/1166467957212054271_space_helmets.json'), require(this.controls));
-                        break;
-                    }
-                    case "galaxy_flyin_3": {
-                        this.HatDatabase = Object.assign({}, require('../public/data/1166467957212054271_galaxy_flyin_3.json'), require(this.controls));
-                        break;
-                    }
-                    case "star_wars_scout_helmet": {
-                        this.HatDatabase = Object.assign({}, require('../public/data/1172247038427922799_star_wars_scout_helmet.json'), require(this.controls));
-                        break;
-                    }
-                    case "samurai_helmets": {
-                        this.HatDatabase = Object.assign({}, require('../public/data/1172272863143527350_samurai_helmets.json'), require(this.controls));
-                        break;
-                    }
-                    case "town_helmets": {
-                        this.HatDatabase = Object.assign({}, require('../public/data/1172957249807582137_town_helmets.json'), require(this.controls));
-                        break;
-                    }
-                    case "viking_helmets": {
-                        this.HatDatabase = Object.assign({}, require('../public/data/1184323616783729170_viking_helmets.json'), require(this.controls));
-                        break;
-                    }
-                    default: { // all - manually combined
-                        this.HatDatabase = Object.assign({}, require('../public/data/all.json'), require(this.controls));
-                        break;
-                    }
-                }
-                this.started();
-            }
+		this.buzzerSound = this.assets.createSound(
+			'alarmSound',
+			{ uri: 'Gameboy Startup Sound.wav' });
 
 
-        });
-        this.context.onUserLeft(user => this.userLeft(user));
+		// Set up cursor interaction. We add the input behavior ButtonBehavior to the button.
+		// Button behaviors have two pairs of events: hover start/stop, and click start/stop.
+		const buttonBehavior = this.button.setBehavior(MRE.ButtonBehavior);
 
-        /* Add a userJoined function to let the syncFix object know a new user has joined. */
-        this.syncFix.autoSynchronizeAttachments(context);
-        this.context.onUserJoined(() => this.syncFix.userJoined());
-    }
+		// Trigger the grow/shrink animations on hover.
+		buttonBehavior.onHover('enter', () => {
+			// use the convenience function "AnimateTo" instead of creating the animation data in advance
+			MRE.Animation.AnimateTo(this.context, this.button, {
+				destination: { transform: { local: { scale: { x: 0.6, y: 0.6, z: 0.6 } } } },
+				duration: 0.3,
+				easing: MRE.AnimationEaseCurves.EaseOutSine
+			});
+		});
+		buttonBehavior.onHover('exit', () => {
+			MRE.Animation.AnimateTo(this.context, this.button, {
+				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
+				duration: 0.3,
+				easing: MRE.AnimationEaseCurves.EaseOutSine
+			});
+		});
 
-    /**
-     * Called when a Hats application session starts up.
-     */
-    private async started() {
-        if(DEBUG){ console.log(this.HatDatabase); }
-        // Show the hat menu.
-        this.showHatMenu();
-    }
+		// When clicked, do a 360 sideways.
+		buttonBehavior.onClick(_ => {
+			
+			this.startSound();
+			MRE.Animation.AnimateTo(this.context, this.button, {
+				destination: { transform: { local: { scale: { x: 2, y: 5, z: 2 } } } },
+				duration: 0.7,
+				easing: MRE.AnimationEaseCurves.EaseOutSine
+			});
+			MRE.Animation.AnimateTo(this.context, this.button, {
+				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
+				duration: 0.3,
+				easing: MRE.AnimationEaseCurves.EaseOutSine
+			});
+		});
 
-    /**
-     * Called when a user leaves the application (probably left the Altspace world where this app is running).
-     * @param user The user that left the building.
-     */
-    private userLeft(user: MRE.User) {
-        // If the user was wearing a hat, destroy it. Otherwise it would be
-        // orphaned in the world.
-        if (this.attachedHats.has(user.id)) { this.attachedHats.get(user.id).destroy(); }
-        this.attachedHats.delete(user.id);
-    }
+	}
+	private startSound = () => {
+		const options: MRE.SetAudioStateOptions = { volume: 0.4 };
+		options.time = 0;
+		this.button.startSound(this.buzzerSound.id, options);
+	}
 
-    /**
-     * Show a menu of hat selections.
-     */
-    private showHatMenu() {
-        // Create a parent object for all the menu items.
-        const menu = MRE.Actor.Create(this.context);
-        let x = 0;
-
-        // check for options first since order isn't guaranteed in a dict
-        for (const k of Object.keys(this.HatDatabase)) {
-            if (k == "options"){
-                const options = this.HatDatabase[k]
-                if (options.previewMargin){
-                    this.previewMargin = options.previewMargin;
-                }
-            }
-        }
-
-        // Loop over the hat database, creating a menu item for each entry.
-        for (const hatId of Object.keys(this.HatDatabase)) {
-            if (hatId == "options") continue; // skip the special 'options' key
-
-            const hatRecord = this.HatDatabase[hatId];
-
-            // Create a clickable button.
-//            var button: MRE.Actor;
-
-            // special scaling and rotation for menu
-            const rotation = hatRecord.menuRotation ? hatRecord.menuRotation : { x: 0, y: 0, z: 0 }
-            const scale = hatRecord.menuScale ? hatRecord.menuScale : { x: 3, y: 3, z: 3 }
-            const position = hatRecord.menuPosition ? hatRecord.menuPosition : { x: 0, y: 1, z: 0 }
-
-            // Create a Artifact without a collider
-            MRE.Actor.CreateFromLibrary(this.context, {
-                resourceId: hatRecord.resourceId,
-                actor: {
-                    transform: {
-                        local: {
-                            position: { x, y: position.y, z: position.z },
-                            rotation: MRE.Quaternion.FromEulerAngles(
-                                rotation.x * MRE.DegreesToRadians,
-                                rotation.y * MRE.DegreesToRadians,
-                                rotation.z * MRE.DegreesToRadians),
-                            scale: scale
-                        }
-                    }
-                }
-            });
-
-            // Create an invisible cube with a collider
-            const button = MRE.Actor.CreatePrimitive(this.assets, {
-                definition: {
-                    shape: MRE.PrimitiveShape.Box,
-                    dimensions: { x: 0.4, y: 0.4, z: 0.4 } // make sure there's a gap
-                },
-                addCollider: true,
-                actor: {
-                    parentId: menu.id,
-                    name: hatId,
-                    transform: {
-                        local: {
-                            position: { x, y: 1, z: 0 },
-                            scale: { x: 3, y: 3, z: 3 } // not affected by custom scale
-                        }
-                    },
-                    appearance: {
-                        enabled: false
-                    }
-                }
-            });
-
-            // Set a click handler on the button.
-            // NOTE: button press event fails on MAC
-            button.setBehavior(MRE.ButtonBehavior).onClick(user => this.wearHat(hatId, user.id));
-
-            this.syncFix.addSyncFunc(() => {
-                    button.setBehavior(MRE.ButtonBehavior).onClick(user => this.wearHat(hatId, user.id))
-            });
-
-
-            //button.setBehavior(MRE.ButtonBehavior).onButton('pressed', user => this.wearHat(hatId, user.id));
-
-            x += this.previewMargin;
-        }
-    }
-
-    /**
-     * Instantiate a hat and attach it to the avatar's head.
-     * @param hatId The id of the hat in the hat database.
-     * @param userId The id of the user we will attach the hat to.
-     */
-    private wearHat(hatId: string, userId: MRE.Guid) {
-        // If the user selected 'clear', then early out.
-        if (hatId == "clear!") {
-            // If the user is wearing a hat, destroy it.
-            if (this.attachedHats.has(userId)) this.attachedHats.get(userId).destroy();
-            this.attachedHats.delete(userId);
-            return;
-        }
-        else if (hatId == "moveup!") {
-            if (this.attachedHats.has(userId))
-                this.attachedHats.get(userId).transform.local.position.y += 0.01;
-            return;
-        }
-        else if (hatId == "movedown!") {
-            if (this.attachedHats.has(userId))
-                this.attachedHats.get(userId).transform.local.position.y -= 0.01;
-            return;
-        }
-        else if (hatId == "moveforward!") {
-            if (this.attachedHats.has(userId))
-                this.attachedHats.get(userId).transform.local.position.z += 0.01;
-            return;
-        }
-        else if (hatId == "moveback!") {
-            if (this.attachedHats.has(userId))
-                this.attachedHats.get(userId).transform.local.position.z -= 0.01;
-            return;
-        }
-        else if (hatId == "sizeup!") {
-            if (this.attachedHats.has(userId)){
-                this.attachedHats.get(userId).transform.local.scale.x += 0.02;
-                this.attachedHats.get(userId).transform.local.scale.y += 0.02;
-                this.attachedHats.get(userId).transform.local.scale.z += 0.02;
-            }
-            return;
-        }
-        else if (hatId == "sizedown!") {
-            if (this.attachedHats.has(userId)){
-                this.attachedHats.get(userId).transform.local.scale.x -= 0.02;
-                this.attachedHats.get(userId).transform.local.scale.y -= 0.02;
-                this.attachedHats.get(userId).transform.local.scale.z -= 0.02;
-            }
-            return;
-        }
-
-        // If the user is wearing a hat, destroy it.
-
-
-        // Create the hat model and attach it to the avatar's head.
-        // Jimmy
-
-        const position = hatRecord.position ? hatRecord.position : { x: 0, y: 0, z: 0 }
-        const scale = hatRecord.scale ? hatRecord.scale : { x: 1.5, y: 1.5, z: 1.5 }
-        const rotation = hatRecord.rotation ? hatRecord.rotation : { x: 0, y: 180, z: 0 }
-        const attachPoint = <MRE.AttachPoint> (hatRecord.attachPoint ? hatRecord.attachPoint : 'head')
-
-        const actor = MRE.Actor.CreateFromLibrary(this.context, {
-            resourceId: hatRecord.resourceId,
-            actor: {
-                transform: {
-                    local: {
-                        position: position,
-                        rotation: MRE.Quaternion.FromEulerAngles(
-                            rotation.x * MRE.DegreesToRadians,
-                            rotation.y * MRE.DegreesToRadians,
-                            rotation.z * MRE.DegreesToRadians),
-                        scale: scale
-                    }
-                },
-                attachment: {
-                    attachPoint: attachPoint,
-                    userId
-                }
-            }
-        });
-
-        this.attachedHats.set(userId, actor);
-    }
 }
